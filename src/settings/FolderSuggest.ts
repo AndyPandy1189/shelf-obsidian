226bed10435ebc41b0ea6a31d03b8a3f3b522bb0
AndyPandy1189 <andrewn.oleary@gmail.com>
2026-08-31 09:31:12 -0500

    chore: remove forbidden eslint-disable comments to fix linter errors and bump to 1.0.5

--- src/settings/FolderSuggest.ts
+++ src/settings/FolderSuggest.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-floating-promises, @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any, @typescript-eslint/no-misused-promises, obsidianmd/prefer-create-el */
 import { App, AbstractInputSuggest, TFolder } from 'obsidian';
 
 export class FolderSuggest extends AbstractInputSuggest<TFolder> {
